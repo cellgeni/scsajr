d5228b8ce70b404b66f55360d87b9b81eafe98ea
Akito Shima <as82@sanger.ac.uk>
2025-07-07 12:25:46 +0100

    Adding more space between coverage/junction plot and transcript plot

--- R/scsajr.R
+++ R/scsajr.R
@@ -2264,7 +2264,10 @@ plot_segment_coverage <- function(
   if (ncol(layout_matrix) > 1) {
     layout_matrix[nrow(layout_matrix), -ncol(layout_matrix)] <- max(layout_matrix) + 1
   }
-  graphics::layout(layout_matrix, widths = c(rep(1, ncol(layout_matrix) - 1), 3), heights = c(rep(1, nrow(layout_matrix) - 1), 6))
+  graphics::layout(layout_matrix,
+    widths = c(rep(1, ncol(layout_matrix) - 1), 3),
+    heights = c(rep(1, nrow(layout_matrix) - 1), 6)
+    )
   graphics::par(bty = "n", tcl = -0.2, mgp = c(1.3, 0.3, 0), mar = c(0, 0.5, 0, 0), oma = oma, xpd = NA)
 
   # 8. Plot CPM boxplot if available
@@ -2352,7 +2355,7 @@ plot_segment_coverage <- function(
   }
 
   # 11. Transcript model plot
-  graphics::par(mar = c(3, 6, 0.2, 0))
+  graphics::par(mar = c(3, 6, 1.5, 0))
   plotCoverage::plotTranscripts(gtf, new = TRUE, exon.col = NA, cds.col = NA, xlim = c(start, stop))
 
   # 12. CPM vs PSI scatter if both present
